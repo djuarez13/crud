e28ff3a046bd6d43196baf79bffdbd2fb4bf3186
DIego <djuarez@trigarante.com>
2020-01-31 17:36:25 -0600

    version finalizada del crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,48 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { TablaComponent } from './components/tabla/tabla.component';
+import { FormularioComponent } from './components/formulario/formulario.component';
+
+// AppRoutingModule
+import { AppRoutingModule } from './app-routing.module';
+
+// AngularMAterial
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatRadioModule} from '@angular/material/radio';
+
+// Formulario
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
+// HttpClient
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TablaComponent,
+    FormularioComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/persona.model.ts
@@ -0,0 +1,7 @@
+export class PersonaModel{
+  id?: string;
+  nombre: string;
+  curp: string;
+  edad: number;
+  sexo = 'mujer';
+}
